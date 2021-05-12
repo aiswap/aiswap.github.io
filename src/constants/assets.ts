5f19fb87b2fda26905f63851ca8ee1c7e3f7adfd
sunbeyond <sunbeyond@gmail.com>
2021-05-12 19:16:11 +0800

    update

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -1,2 +1,2 @@
 // submodule git@github.com:aiswap/assets.git
-export const LocalTokenIconPath = ({address}: {address: string | undefined}) => `/images/assets/main/OKExChain/${address}.png`
+export const LocalTokenIconPath = ({address}: {address: string | undefined}) => `/images/assets/main/OKExChain/${(address + '').toLocaleLowerCase()}.png`
